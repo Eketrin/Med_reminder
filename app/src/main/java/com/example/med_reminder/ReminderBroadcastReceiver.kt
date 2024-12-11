d64fac0a15b2ebe5050cfb901bf88e14e8255968
Eketrin <Katebaiko@yandex.ru>
2024-12-11 23:00:42 +0300

    доп сохранение

--- app/src/main/java/com/example/med_reminder/ReminderBroadcastReceiver.kt
+++ app/src/main/java/com/example/med_reminder/ReminderBroadcastReceiver.kt
@@ -1,5 +1,6 @@
 package com.example.med_reminder
 
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -23,18 +24,30 @@ class ReminderBroadcastReceiver : BroadcastReceiver() {
             // Получаем экземпляр NotificationManagerCompat для управления уведомлениями
             val notificationManager = NotificationManagerCompat.from(context)
 
+            // Создаем Intent для удаления уведомления
+            val dismissIntent = Intent(context, DismissNotificationReceiver::class.java).apply {
+                putExtra("id", id) // Передаем идентификатор уведомления
+            }
+            val dismissPendingIntent = PendingIntent.getBroadcast(
+                context,
+                id ?: 0, // Уникальный идентификатор для PendingIntent
+                dismissIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE // Флаги для PendingIntent
+            )
+
             // Создаем уведомление с использованием NotificationCompat.Builder
             val builder = NotificationCompat.Builder(context, ReminderApplication.channelId)
                 .setSmallIcon(R.drawable.ic_notification)
-                .setContentTitle(context.resources.getString(R.string.new_reminder)) //заголовок
+                .setContentTitle(context.resources.getString(R.string.new_reminder)) // Заголовок
                 .setContentText(text) // Устанавливаем текст уведомления
-                .setPriority(NotificationCompat.PRIORITY_HIGH) //приоритет
-                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC) //видимость
-                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)) //звук
+                .setPriority(NotificationCompat.PRIORITY_HIGH) // Приоритет
+                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC) // Видимость
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)) // Звук
+                .addAction(R.drawable.ic_read, "Прочитано", dismissPendingIntent) // Кнопка "Прочитано"
 
             // Проверка на разрешение на отправку уведомлений
             if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
-                return // Если нет выходим из метода
+                return // Если нет, выходим из метода
             }
 
             // Отправляем уведомление
@@ -42,3 +55,16 @@ class ReminderBroadcastReceiver : BroadcastReceiver() {
         }
     }
 }
+
+// Новый BroadcastReceiver для обработки нажатия на кнопку "Прочитано"
+class DismissNotificationReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (context != null && intent != null) {
+            val id = intent.getIntExtra("id", 0)
+            val notificationManager = NotificationManagerCompat.from(context)
+            notificationManager.cancel(id) // Удаляем уведомление с указанным id
+        }
+    }
+}
+
+
--- app/src/main/java/com/example/med_reminder/ReminderItem.kt
+++ app/src/main/java/com/example/med_reminder/ReminderItem.kt
@@ -15,8 +15,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -80,6 +83,14 @@ fun ReminderItem(reminder: Reminder, viewModel: RemindersViewModel) {
                 style = TextStyle(color = colorResource(id = R.color.green)),
                 modifier = Modifier.padding(start = 7.dp)
             )
+
+
+//            Checkbox(
+//                checked = isChecked,
+//                onCheckedChange = { isChecked = it }, // Обработка изменения состояния чекбокса
+//                modifier = Modifier.padding(start = 7.dp)
+//            )
+
             Image(
                 painter = painterResource(id = R.drawable.delete),
                 contentDescription = "Delete",
@@ -92,5 +103,6 @@ fun ReminderItem(reminder: Reminder, viewModel: RemindersViewModel) {
                     }
             )
         }
+
     }
 }
